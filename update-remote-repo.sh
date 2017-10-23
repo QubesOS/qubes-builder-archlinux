92bcd5049447e5d4350a65a4e236f3a635b9af2f
Olivier MEDOC <o_medoc@yahoo.fr>
2017-10-23 09:33:09 +0200

    allow creating repository for Qubes 4.0

--- update-remote-repo.sh
+++ update-remote-repo.sh
@@ -30,10 +30,22 @@ sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for
 sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes*3.2.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r3.2.db.tar.gz "$pkg";done;'
 
 # Include XEN 4.6.*
-sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes-vm-xen-4.6.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r3.1.db.tar.gz "$pkg";done;'
+sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes-vm-xen-4.6.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r3.2.db.tar.gz "$pkg";done;'
+
+## release 4.0 Repository
+echo "Generating 4.0 repository"
+
+# Include 3.0 3.1 3.2 and 4.0 packages
+sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes*3.0.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r4.0.db.tar.gz "$pkg";done;'
+sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes*3.1.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r4.0.db.tar.gz "$pkg";done;'
+sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes*3.2.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r4.0.db.tar.gz "$pkg";done;'
+sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes*4.0.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r4.0.db.tar.gz "$pkg";done;'
+
+# Include XEN 4.8.*
+sudo chroot chroot-archlinux su user -c 'cd /tmp/qubes-packages-mirror-repo; for pkg in `ls -v pkgs/qubes-vm-xen-4.8.*.pkg.tar.xz` ; do repo-add pkgs/qubes-r4.0.db.tar.gz "$pkg";done;'
 
 echo "Signing all repositories"
-for repo in "qubes-r3.1" "qubes-r3.2"; do
+for repo in "qubes-r3.1" "qubes-r3.2" "qubes-r4.0"; do
 	echo "Signing repository $repo"
 	# Replace link with the real thing because it cannot be uploaded easily to repository
 	rm qubes-packages-mirror-repo/archlinux/pkgs/${repo}.db
