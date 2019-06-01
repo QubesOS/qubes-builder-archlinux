04a77e4a6830c56387fb8582f8936e7ca3a0aff1
Rob Cohen <rob@rcohen.me>
2019-06-01 01:11:55 -0400

    Fixed minor error

--- scripts/00_prepare.sh
+++ scripts/00_prepare.sh
@@ -1,12 +1,10 @@
-#!/bin/bash -e
-# vim: set ts=4 sw=4 sts=4 et :
-### 00_prepare.sh : Download and extract the archlinux bootstrap mini-distro
+#! /bin/bash
+
 echo "--> Archlinux 00_prepare.sh"
 
 ARCHLINUX_PLUGIN_DIR="${ARCHLINUX_PLUGIN_DIR:-"${SCRIPTSDIR}/.."}"
-
 ARCHLINUX_SRC_PREFIX="${ARCHLINUX_SRC_PREFIX:-http://mirrors.kernel.org/archlinux}"
-BOOTSTRAP_TARBALL=$(wget -qO- $ARCHLINUX_SRC_PREFIX/iso/latest/sha1sums.txt | grep bootstrap | awk '{print $2}')
+BOOTSTRAP_TARBALL=$(wget -qO- "$ARCHLINUX_SRC_PREFIX"/iso/latest/sha1sums.txt | grep bootstrap | awk '{print $2}')
 
 BOOTSTRAP_URL="${ARCHLINUX_SRC_PREFIX}/iso/latest/${BOOTSTRAP_TARBALL}"
 GPG_ENV="GNUPGHOME=${CACHEDIR}/gpghome"
@@ -23,10 +21,10 @@ http_proxy="$REPO_PROXY" wget -N -P "$CACHEDIR" "${BOOTSTRAP_URL}.sig"
 echo "  --> Preparing GnuPG to verify tarball..."
 mkdir -p "${CACHEDIR}/gpghome"
 chmod -R go-rwx "${CACHEDIR}/gpghome"
-env $GPG_ENV gpg --import "${ARCHLINUX_PLUGIN_DIR}/keys/archlinux-master-keys.asc" || exit
+env "$GPG_ENV" gpg --import "${ARCHLINUX_PLUGIN_DIR}/keys/archlinux-master-keys.asc" || exit
 
 echo "  --> Verifying tarball..."
-env $GPG_ENV gpg --verify "${CACHEDIR}/${BOOTSTRAP_TARBALL}.sig" "${CACHEDIR}/${BOOTSTRAP_TARBALL}" || exit
+env "$GPG_ENV" gpg --verify "${CACHEDIR}/${BOOTSTRAP_TARBALL}.sig" "${CACHEDIR}/${BOOTSTRAP_TARBALL}" || exit
 
 if [ "${CACHEDIR}/${BOOTSTRAP_TARBALL}" -nt "${CACHEDIR}/bootstrap/.extracted" ]; then
     echo "  --> Extracting bootstrap tarball (nuking previous directory)..."
