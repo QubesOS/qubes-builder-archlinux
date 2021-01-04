3575253d655bf02b1b805ad2fae9b31cdd4111a1
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2021-01-04 06:38:25 +0100

    Do not make volatile private keys dir for package builds themselves
    The issue with pacman private key sharing is relevant for TemplateVM (or
    generally - VMs) only, not the build chroot itself. Skip this volatile
    part for the builded itself, to not break installing repositories. This
    includes update-local-repo.sh script, which installs archlinux-keyring

--- scripts/01_install_core.sh
+++ scripts/01_install_core.sh
@@ -8,4 +8,8 @@ ARCHLINUX_PLUGIN_DIR="${ARCHLINUX_PLUGIN_DIR:-"${SCRIPTSDIR}/.."}"
 set -e
 [ "$VERBOSE" -ge 2 -o "$DEBUG" -gt 0 ] && set -x
 
+# make sure pacman master private key is _not_ stored in the TemplateVM - see
+# scripts/arch-chroot-lite for details
+unset SKIP_VOLATILE_SECRET_KEY_DIR
+
 "${ARCHLINUX_PLUGIN_DIR}/prepare-chroot-base" "$INSTALLDIR" "$DIST"
