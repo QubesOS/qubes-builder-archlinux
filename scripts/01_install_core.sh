0d8e2154112a34b326096cc5cea8082bf12f67bd
unman <unman@thirdeyesecurity.org>
2021-05-11 15:08:42 +0000

    Include offical Qubes repositories

--- scripts/01_install_core.sh
+++ scripts/01_install_core.sh
@@ -12,4 +12,11 @@ set -e
 # scripts/arch-chroot-lite for details
 unset SKIP_VOLATILE_SECRET_KEY_DIR
 
+# Now there is an official Qubes repository install the Qubes key
+"${SCRIPTSDIR}/arch-chroot-lite" "$INSTALLDIR" pacman-key --add - < \
+    "${ARCHLINUX_PLUGIN_DIR}/keys/qubes-repo-archlinux-key.asc"
+key_fpr=$(gpg --with-colons --show-key "${ARCHLINUX_PLUGIN_DIR}/keys/qubes-repo-archlinux-key.asc"|\
+            grep ^fpr: | cut -d : -f 10)
+"${SCRIPTSDIR}/arch-chroot-lite" "$INSTALLDIR" pacman-key --lsign "$key_fpr"
+
 "${ARCHLINUX_PLUGIN_DIR}/prepare-chroot-base" "$INSTALLDIR" "$DIST"
